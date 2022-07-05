748c4f345a9f20c4709f1e0c1cb15fa83eced1fb
Ayushi6Sharma <65151475+Ayushi6Sharma@users.noreply.github.com>
2022-07-05 10:13:06 +0530

    commit

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,7 +5,7 @@ export default function Welcome(props)
    
     return(
     <>
-        Hey ! <h1>{props.name}</h1>
+       Hey !<h1>{props.name}</h1>
         <h2>Welcome to Newton School.</h2>
     </>
     );
